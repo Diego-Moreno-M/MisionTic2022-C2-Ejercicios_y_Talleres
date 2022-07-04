e9fa2fdf3bcdc5e0c256ed76c0fd7c68296ee002
Diego Moreno <diegoarmando.moreno@outlook.com>
2022-07-04 18:53:46 -0500

    Cambios y mas ejercicios

--- Ejercicios-Java/Sentencias.java
+++ Ejercicios-Java/Sentencias.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Sentencias {
     public static void main(String[] args) {
-        condicionales2();
+        ciclos2();
     }
 
     private static void condicionales() {
